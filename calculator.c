c1a3ad67d3fd10a0543dd0cbb88e3a33b09bba76
강의실컴 <ghearts4239@gmail.com>
2025-04-10 20:44:04 +0900

    토대 구축

--- calculator.c
+++ calculator.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
+int calculation();
+
 int main()
+{
+    int selection;
+    printf("Calculator\n기능 선택\n1.계산기\n2.기록 조회\n3.기록 삭제\n4.종료\n");
+    scanf("%d", &selection);
+    switch (selection)
+    {
+    case 1:
+        calculation();
+
+    default:
+        printf("구현중\n");
+        break;
+    }
+}
+
+int calculation()
 {
     char mathematicalExpression[10] = {0};
     printf("Calculator\n");
