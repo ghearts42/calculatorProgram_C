decb2b6accc296b7a0045d146e4685ee3804e370
강의실컴 <ghearts4239@gmail.com>
2025-04-10 20:25:14 +0900

    Basic

---
+++ calculator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char mathematicalExpression[10] = {0};
+    printf("Calculator\n");
+    scanf("%s", mathematicalExpression);
+    printf("%s\n", mathematicalExpression);
+}
